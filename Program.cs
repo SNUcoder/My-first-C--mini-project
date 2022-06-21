b762281d76cebccb9ce810038a3a050b3f0d5123
Naveed <98022575+SNUcoder@users.noreply.github.com>
2022-06-21 19:32:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tutorial
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "SNUcoder";
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.WindowHeight = 40;
+			Console.WindowWidth = 40;
+
+			Console.WriteLine("Hello, what's your name?");
+
+			Console.ReadLine();
+			
+			Console.WriteLine("I am ATeX your friendly assistant\n I was made by ATeXTeam in 2060");
+			Console.WriteLine("\n\nWhat is your favourite food?");
+			
+			Console.ReadLine();
+
+			Console.WriteLine("Tasty! Mine is chocolate with a  pinch of RAM (only a little bit of RAM)");
+
+			Console.ReadLine();
+		}
+	}
+}
